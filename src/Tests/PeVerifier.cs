583f01eee2737bba72e1fb0338eb166bc5018139
Simon Cropp <simon.cropp@gmail.com>
2021-11-06 15:59:47 +1100

    .

---
+++ src/Tests/PeVerifier.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+public static class PeVerifier
+{
+    public static readonly bool FoundPeVerify;
+    static string? peverifyPath;
+
+    static PeVerifier()
+    {
+        FoundPeVerify = SdkToolFinder.TryFindTool("peverify", out peverifyPath);
+    }
+
+    public static string TrimLineNumbers(string input)
+    {
+        return Regex.Replace(input, @"\[offset .*\]", "");
+    }
+
+  public  static bool Verify(
+        string assemblyPath,
+        List<string> ignoreCodes,
+        out string output)
+    {
+        ignoreCodes.Add("0x80070002");
+        ignoreCodes.Add("0x80131252");
+        var workingDirectory = Path.GetDirectoryName(assemblyPath);
+        var processStartInfo = new ProcessStartInfo(peverifyPath!)
+        {
+            Arguments = $"\"{assemblyPath}\" /hresult /nologo /ignore={string.Join(",", ignoreCodes)}",
+            WorkingDirectory = workingDirectory!,
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            RedirectStandardOutput = true
+        };
+
+        using var process = Process.Start(processStartInfo);
+        output = process!.StandardOutput.ReadToEnd();
+        output = Regex.Replace(output, "^All Classes and Methods.*", "");
+        output = output.Trim();
+        output = TrimLineNumbers(output);
+        if (!process.WaitForExit(10000))
+        {
+            throw new Exception("PeVerify failed to exit");
+        }
+
+        if (process.ExitCode != 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/Tests/SdkToolFinder.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+static class SdkToolFinder
+{
+    static string windowsSdkDirectory;
+
+    static SdkToolFinder()
+    {
+        var programFilesPath = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+        windowsSdkDirectory = Path.Combine(programFilesPath, @"Microsoft SDKs\Windows");
+        if (Directory.Exists(windowsSdkDirectory))
+        {
+            foundToolsDirectory = true;
+        }
+    }
+
+    static bool foundToolsDirectory;
+
+    public static bool TryFindTool(string tool, [NotNullWhen(true)] out string? path)
+    {
+        if (!foundToolsDirectory)
+        {
+            path = null;
+            return false;
+        }
+
+        path = Directory.EnumerateFiles(windowsSdkDirectory, $"{tool}.exe", SearchOption.AllDirectories)
+            .Where(x => !x.ToLowerInvariant().Contains("x64"))
+            .OrderByDescending(x =>
+            {
+                var info = FileVersionInfo.GetVersionInfo(x);
+                return new Version(info.FileMajorPart, info.FileMinorPart, info.FileBuildPart);
+            })
+            .FirstOrDefault();
+        return path != null;
+    }
+}
